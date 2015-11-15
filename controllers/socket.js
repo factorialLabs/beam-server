a3871b87c0916132af7ab955d0050ce8e70b0b30
jgzuke <jgzuke@gmail.com>
2015-11-15 14:00:06 -0500

    beam to specific users

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,6 +1,7 @@
 "use strict";
 var secrets = require('../config/secrets');
 var socketioJwt = require('socketio-jwt');
+var userIds = {};
 
 var Socket = {
 	setIo: function(io){
@@ -10,10 +11,13 @@ var Socket = {
         })).on('authenticated', function(socket) {
             //this socket is authenticated, we are good to handle more events from it.
             console.log('signed in: ' + socket.decoded_token.email);
+            
+            userIds[socket.decoded_token.email] = socket.id;
 
             socket.on('beam tab', function(beam){
                 console.log("incoming beam", beam);
-                io.emit('incoming beam', beam);
+                var recipient = userIds[beam.message];
+                io.to(recipient).emit('incoming beam', beam);
             });
             socket.on('sign in', function(socket){
                 console.log("user signed in");
