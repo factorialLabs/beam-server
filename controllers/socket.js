066282351b943146df9cbc235aa4aa908c2e7b5c
Deon Hua <deonhbci@gmail.com>
2015-11-14 21:10:43 -0500

    Messages.

--- controllers/socket.js
+++ controllers/socket.js
@@ -2,11 +2,12 @@
 var Socket = {
 	setIo: function(io){
 		io.on("connection", function(socket){
-			console.log('connection', socket);
+			console.log('connection');
 		    io.emit('incoming beam', {url: 'http://techretreat.ca'});
 		    
-		    socket.on('beam tab', function(socket){
-		    	io.emit('incoming beam', {url: socket.url});
+		    socket.on('beam tab', function(beam){
+		    	console.log("incoming beam", beam);
+		    	io.emit('incoming beam', beam);
 		    });
 		    socket.on('sign in', function(socket){
 		    	console.log("user signed in");
