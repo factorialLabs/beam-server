094ce70f2d467e0c8b557bbb791fd4164ebbb5d1
Yu Chen Hou <houyuchen66@gmail.com>
2015-11-25 01:11:39 -0500

    friend request server logic

--- controllers/socket.js
+++ controllers/socket.js
@@ -5,33 +5,52 @@ var User = require('../models/User');
 
 var userIds = {};
 
+
 var Socket = {
-	setIo: function(io){
-		io.on("connection", socketioJwt.authorize({
-            secret: secrets.jwt.secretOrKey,
-            timeout: 15000 // 15 seconds to send the authentication message
-        })).on('authenticated', function(socket) {
-            //this socket is authenticated, we are good to handle more events from it.
-            console.log('signed in: ' + socket.decoded_token.email);
-            
-            userIds[socket.decoded_token.email] = socket.id;
+  broadcastStatusToFriends: function(user, status){
+    user.friends.forEach(function(friend, index) {
+      var recipient = userIds[friend.email];
+      io.to(recipient).emit('friend status', status);
+    });
+  },
 
-            socket.on('beam tab', function(beam){
-                console.log("incoming beam", beam);
-                var recipient = userIds[beam.recipient];
-                io.to(recipient).emit('incoming beam', beam);
-                User.findOne()
-            });
+  setIo: function(io){
+    io.on("connection", socketioJwt.authorize({
+      secret: secrets.jwt.secretOrKey,
+      timeout: 15000 // 15 seconds to send the authentication message
+    })).on('authenticated', function(socket) {
+      //this socket is authenticated, we are good to handle more events from it.
+      console.log('signed in: ' + socket.decoded_token.email);
 
-            socket.on('sign in', function(socket){
-                console.log("user signed in");
-            });
+      userIds[socket.decoded_token.email] = socket.id;
 
-            socket.on('disconnect', function(socket){
-                console.log("user disconnected.");
-            });
-        });
-	}
+      socket.on('beam tab', function(beam){
+        console.log("incoming beam", beam);
+        var recipient = userIds[beam.recipient];
+        io.to(recipient).emit('incoming beam', beam);
+      });
+
+      socket.on('sign in', function(socket){
+        console.log("user signed in");
+      });
+
+      socket.on('away status change', function(socket){
+        console.log("user is away");
+      });
+
+      socket.on('send friend invite', function(socket){
+        console.log("user is sending friend invite");
+      });
+
+      socket.on('accept friend invite', function(socket){
+        console.log("user accepted friend invite");
+      });
+
+      socket.on('disconnect', function(socket){
+        console.log("user disconnected.");
+      });
+    });
+  }
 };
 
-module.exports = Socket; 
+module.exports = Socket;
