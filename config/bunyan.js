47af03ae9287fbf3898688559ce8bf4b7e8012ca
Deon Hua <deonhbci@gmail.com>
2016-11-11 21:37:39 -0800

    Bunyan logs

---
+++ config/bunyan.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    name: 'beam',
+    level: 'debug',
+  },
+  test: {
+    name: 'beam',
+    level: 'debug',
+  },
+  staging: {
+    name: 'beam',
+    level: 'info',
+  },
+  production: {
+    name: 'beam',
+    level: 'info',
+  },
+};
