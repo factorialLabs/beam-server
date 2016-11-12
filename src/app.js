179a8efa1facec2807645f631083c4d7e25e7f46
Deon Hua <deonhbci@gmail.com>
2016-11-11 21:38:44 -0800

    Remove extra logging

--- src/app.js
+++ src/app.js
@@ -113,5 +113,4 @@ http.listen(app.get('port'), function(){
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
-log.info('booted up!');
 module.exports = app;
