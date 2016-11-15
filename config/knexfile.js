0e08e5c275914c39444e23dd54d40cb29ccf7a33
Deon Hua <me@deonhua.com>
2016-11-15 04:23:15 -0500

    Run DB Migrations on Travis (#15)
    * Run DB Migrations on Travis
    
    * Env var

--- config/knexfile.js
+++ config/knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
       host: '127.0.0.1',
       port: 5432,
       user: 'postgres',
-      password: 'password',
+      password: '',
       database: 'beam_test',
       debug: true,
     },
